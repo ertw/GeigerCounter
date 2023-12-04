b77d90bcfe97db5f04e8777e77fcbafc08046f27
Erik Williamson <erik.williamson@applydigital.com>
2023-12-03 18:46:17 -0800

    Sort by date and print results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,58 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        final String fileName = "/Users/erikwilliamson/Documents/CSC142/GeigerCounter/src/main/java/7_14_2019.txt";
+    private static final String FILE_NAME = "path/to/file.txt";
+
+    private static void printSample(RadiationSample sample) {
+        System.out.printf("%-12s%-10d%n", sample.getDateTime(), sample.getCountsPerMinute());
+    }
+
+    private static LinkedList<RadiationSample> parseRadiationSamples(String fileName) {
         final LinkedList<RadiationSample> radiationSamples = new LinkedList<>();
+        final String dtRegex = "^(\\d{1,2}/){1,2}\\d{4}";
+        final Pattern dtPattern = Pattern.compile(dtRegex);
+        final String countsRegex = ",(\\d+),";
+        final Pattern countsPattern = Pattern.compile(countsRegex);
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String dtRegex = "^(\\d{1,2}/){1,2}\\d{4}";
-            Pattern dtPattern = Pattern.compile(dtRegex);
-            String countsRegex = ",(\\d+),";
-            Pattern countsPattern = Pattern.compile(countsRegex);
             String line;
             while ((line = reader.readLine()) != null) {
                 Matcher dtMatcher = dtPattern.matcher(line);
                 Matcher countsMatcher = countsPattern.matcher(line);
-                if (dtMatcher.find()) {
-                    if (countsMatcher.find()) {
-                        radiationSamples.add(new RadiationSample(dtMatcher.group(), Integer.parseInt(countsMatcher.group(1))));
-                    }
+                if (dtMatcher.find() && countsMatcher.find()) {
+                    radiationSamples.add(new RadiationSample(dtMatcher.group(), Integer.parseInt(countsMatcher.group(1))));
                 }
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        radiationSamples.forEach(sample -> System.out.println("Date and Time : " + sample.getDateTime() + "\nCounts Per Minute: " + sample.getCountsPerMinute()));
+        return radiationSamples;
+    }
+
+    private static int compareRadiationSamplesByDateTime(RadiationSample a, RadiationSample b) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        return LocalDate.parse(a.getDateTime(), formatter).isBefore(LocalDate.parse(b.getDateTime(), formatter)) ? -1 : 1;
+    }
+
+    public static void main(String[] args) {
+        final String fileName = args.length > 0 ? args[0] : FILE_NAME;
+        final LinkedList<RadiationSample> radiationSamples = parseRadiationSamples(fileName);
+        radiationSamples.sort(Main::compareRadiationSamplesByDateTime);
+        RadiationSample maxCount = radiationSamples.stream().max((a, b) -> a.getCountsPerMinute() - b.getCountsPerMinute()).orElse(null);
+        System.out.printf("%-12s%-10s%n", "Date", "Counts per minute");
+        System.out.printf("%-12s%-10s%n", "----------  ", "-----------------");
+        if (maxCount != null) {
+            for (RadiationSample sample : radiationSamples) {
+                if (sample.getCountsPerMinute() > maxCount.getCountsPerMinute() - 5) {
+                    printSample(sample);
+                }
+            }
+        }
     }
 }
